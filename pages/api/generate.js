727b9c6840a74b219d9fca9c6d7323a79d94b1e5
Flow-Up12 <marcosje2005@icloud.com>
2023-07-02 14:22:56 -0500

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,44 +7,38 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
-    res.status(500).json({
+    return res.status(500).json({
       error: {
         message: "OpenAI API key not configured, please follow instructions in README.md",
       }
     });
-    return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
+  const animal = req.body.animal?.trim();
+  if (!animal) {
+    return res.status(400).json({
       error: {
         message: "Please enter a valid animal",
       }
     });
-    return;
   }
 
   try {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo-16k",
-      messages: [{role: "user", content: animal}],
-    }); 
-
-    console.log(completion.data.choices[0].message);
+      messages: [{ role: "user", content: animal }],
+    });
 
     const apiResponse = completion.data.choices[0].message.content;
-    console.log(completion.data.choices[0].message); // Log the API response to the console
 
-    res.status(200).json({ result: apiResponse });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+    return res.status(200).json({ result: apiResponse });
+  } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      return res.status(error.response.status).json(error.response.data);
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
+      return res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
         }
