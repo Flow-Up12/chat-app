6c877340dfef2cf337065e5c6b3146235a77fa4b
Flow-Up12 <marcosje2005@icloud.com>
2023-07-02 01:57:27 -0500

    hopefully final

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,44 +7,43 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
-    res.status(500).json({
+    return res.status(500).json({
       error: {
         message: "OpenAI API key not configured, please follow instructions in README.md",
       }
     });
-    return;
   }
 
   const animal = req.body.animal || '';
   if (animal.trim().length === 0) {
-    res.status(400).json({
+    return res.status(400).json({
       error: {
         message: "Please enter a valid animal",
       }
     });
-    return;
   }
 
   try {
-    const completion = await openai.createChatCompletion({
+    const completionPromise = openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{role: "user", content: animal}],
-    }); 
+    });
 
-    console.log(completion.data.choices[0].message);
+    const [completion] = await Promise.all([completionPromise, new Promise(resolve => setTimeout(resolve, 800))]);
 
     const apiResponse = completion.data.choices[0].message.content;
+
     console.log(completion.data.choices[0].message); // Log the API response to the console
 
-    res.status(200).json({ result: apiResponse });
+    return res.status(200).json({ result: apiResponse });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      return res.status(error.response.status).json(error.response.data);
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
+      return res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
         }
--- pages/index.js
+++ pages/index.js
@@ -1,103 +1,102 @@
-  import Head from "next/head";
-  import { useState } from "react";
-  import styles from "./index.module.css";
-  import "highlight.js/styles/github.css";
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import styles from "./index.module.css";
+import "highlight.js/styles/github.css";
 import hljs from "highlight.js";
 
+export default function Home() {
+  const [animalInput, setAnimalInput] = useState("");
+  const [messages, setMessages] = useState([]);
 
-  
+  useEffect(() => {
+    hljs.highlightAll();
+  }, [messages]);
 
-  export default function Home() {
-    const [animalInput, setAnimalInput] = useState("");
-    const [messages, setMessages] = useState([]);
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ animal: animalInput }),
+      });
 
-    async function onSubmit(event) {
-      event.preventDefault();
-      try {
-        const response = await fetch("/api/generate", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ animal: animalInput }),
-        });
-
-        const data = await response.json();
-        if (response.status !== 200) {
-          throw new Error(`Request failed with status ${response.status}`);
-        }
-
-        const newMessage = { content: animalInput, role: "user" };
-        const newResponse = { content: data.result, role: "assistant" };
-
-        // Check if the response has a code block
-        const hasCodeBlock = data.result.includes("```");
-        if (hasCodeBlock) {
-          const codeContent = data.result.replace(/```([\s\S]+?)```/g, '</p><pre><code class="${styles.code} hljs">$1</code></pre><p>');
-          newResponse.content = `<p>${codeContent}</p>`;
-        } else {
-          // Split the response into paragraphs
-          const paragraphs = data.result.split("\n\n");
-          const formattedContent = paragraphs.map((paragraph) => `<p>${paragraph}</p>\n`).join("");
-          newResponse.content = formattedContent;
-        }
-
-        setMessages((prevMessages) => [...prevMessages, newMessage, newResponse]);
-        setAnimalInput("");
-        setTimeout(() => {
-          hljs.highlightAll();
-        }, 10);
-      } catch (error) {
-        console.error(error);
-        alert(error.message);
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw new Error(`Request failed with status ${response.status}`);
       }
-    }
-    
-    
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>Chat app</title>
-          <link rel="icon" href="/ChatGPT_logo.png" />
-        </Head>
 
-        <main className={styles.main}>
-          <img src="/ChatGPT_logo.png" className={styles.icon} alt="ChatGPT Logo" />
-          <h3 classNamcde={styles.idk}>Ask anything</h3>
-          <div className={styles.messageContainer}>
-            {messages.map((message, index) => (
+      const newMessage = { content: animalInput, role: "user" };
+      const newResponse = { content: data.result, role: "assistant" };
+
+      // Check if the response has a code block
+      const hasCodeBlock = data.result.includes("```");
+      if (hasCodeBlock) {
+        const codeContent = data.result.replace(/```([\s\S]+?)```/g, '</p><pre><code class="${styles.code} hljs">$1</code></pre><p>');
+        newResponse.content = `<p>${codeContent}</p>`;
+      } else {
+        // Split the response into paragraphs
+        const paragraphs = data.result.split("\n\n");
+        const formattedContent = paragraphs.map((paragraph) => `<p>${paragraph}</p>\n`).join("");
+        newResponse.content = formattedContent;
+      }
+
+      setMessages((prevMessages) => [...prevMessages, newMessage, newResponse]);
+      setAnimalInput("");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Chat app</title>
+        <link rel="icon" href="/ChatGPT_logo.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/ChatGPT_logo.png" className={styles.icon} alt="ChatGPT Logo" />
+        <h3 className={styles.idk}>Ask anything</h3>
+        <div className={styles.messageContainer}>
+          {messages.map((message, index) => (
             <div
               key={index}
               className={`${styles.message} ${styles[message.role]}`}
             >
               {message.role === "assistant" && (
-                <div className={styles.assistant} >
+                <div className={styles.assistant}>
                   <img src="/ChatGPT_logo.png" className={styles.icon} alt="ChatGPT Logo" />
-                  <div className={styles.content} dangerouslySetInnerHTML={{ __html: message.content }} />
+                  {message.content.includes("<") ? (
+                    <div
+                      className={styles.content}
+                      dangerouslySetInnerHTML={{ __html: message.content }}
+                    />
+                  ) : (
+                    <div>{message.content}</div>
+                  )}
                 </div>
               )}
               {message.role !== "assistant" && (
-                <div dangerouslySetInnerHTML={{ __html: message.content }} />
+                <div>{message.content}</div>
               )}
             </div>
           ))}
-          </div>
-          <form onSubmit={onSubmit} className={styles.form}>
+        </div>
+        <form onSubmit={onSubmit} className={styles.form}>
           <textarea
             name="animal"
             placeholder="Enter your question"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
             className={styles.input}
-          />  
-          <input type="image" src="/enter.png" value="Generate Response" alt="submit" className={styles.button}/>
+          />
+          <input type="image" src="/enter.png" value="Generate Response" alt="submit" className={styles.button} />
         </form>
-        </main>
-        
-      
-
-        
-      </div>
-      
-    );
-  }
+      </main>
+    </div>
+  );
+}
