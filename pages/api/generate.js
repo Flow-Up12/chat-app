42b709c7a96153314c6ba3967db8087bbf07d55c
Flow-Up12 <marcosje2005@icloud.com>
2023-07-02 01:08:34 -0500

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,11 +1,11 @@
-import { Configuration, OpenAIApi } from "openai/dist";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -29,12 +29,16 @@ export default async function (req, res) {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{role: "user", content: animal}],
-    });
+    }); 
+
+    console.log(completion.data.choices[0].message);
 
     const apiResponse = completion.data.choices[0].message.content;
+    console.log(completion.data.choices[0].message); // Log the API response to the console
 
     res.status(200).json({ result: apiResponse });
   } catch(error) {
+    // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
